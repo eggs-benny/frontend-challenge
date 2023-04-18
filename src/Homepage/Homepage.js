b77ff2fa53fc22729317f2b079b15960c4ddc833
eggs-benny <benedict.smith87@gmail.com>
2023-04-18 09:58:39 +0100

    added mui, added grid & boxes (initial), added watermark

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,13 +1,51 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
 import { Post } from '../Post/Post';
+import { Grid } from '@mui/material';
+
+const Watermark = styled('div')({
+  color: 'grey',
+  padding: '5px',
+  fontSize: '12px',
+  fontWeight: 'bolder',
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+});
 
 export function Homepage() {
   return (
     <>
       <h1>Hello World</h1>
-      <Post />
-      <div className="watermark">
-        <p>c WYSPR 2022</p>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+        <Grid item xs={3} sx={{ border: 1 }}>
+          <Post />
+        </Grid>
+      </Grid>
+      <Watermark>
+        <p>© WYSPR 2022</p>
+      </Watermark>
     </>
   );
 }
